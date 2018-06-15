1025fd0dca45b6f960b967c13e50e837284d3158
mirekSBCS <33669390+mirekSBCS@users.noreply.github.com>
2018-06-15 12:03:06 -0400

    Initial Commit
    6/15

---
+++ Learn to Code - Unity/Assets/Scripts/HUD_Manager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+using UnityEngine.UI;
+
+public class HUD_Manager : MonoBehaviour {
+
+	public int score;
+	public Text ScoreText;
+	public ThirdPersonCharacter ThirdPersonCharacter;
+
+	// Use this for initialization
+	void Start () {
+		ThirdPersonCharacter = GameObject.Find ("Player").GetComponent<ThirdPersonCharacter> ();
+		score = 0;
+		ScoreText.text = "Score: " + score;
+	}
+	
+	// Update is called once per frame
+	public void UpdateScore (int points) {
+		score = score + points;
+		ScoreText.text = "Score: " + score;
+	}
+
+	public void UpdateStats (float JumpIncrease, float SpeedIncrease){
+		ThirdPersonCharacter.m_JumpPower += JumpIncrease;
+		ThirdPersonCharacter.m_MoveSpeedMultiplier += SpeedIncrease;
+	}
+}
---
+++ Learn to Code - Unity/Assets/Scripts/Pickup_Gold.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup_Gold : MonoBehaviour {
+
+	public int points;
+	public HUD_Manager HUD_Manager;
+	public float JumpIncrease;
+	public float SpeedIncrease;
+
+	// Use this for initialization
+	void Start () {
+		HUD_Manager = GameObject.Find ("Player").GetComponent<HUD_Manager> ();
+	}
+	
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.CompareTag ("Player")) {
+			gameObject.SetActive(false);
+			HUD_Manager.UpdateScore(points);
+			HUD_Manager.UpdateStats(JumpIncrease, SpeedIncrease);
+		}
+	}
+}
